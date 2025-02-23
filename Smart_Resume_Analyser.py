edb849e52934b3cfd8b89bd3f9eee8698be926dc
arya54 <144202872+arya54@users.noreply.github.com>
2025-02-24 00:43:20 +0530

    Update Smart_Resume_Analyser.py

--- Smart_Resume_Analyser.py
+++ Smart_Resume_Analyser.py
@@ -12,7 +12,7 @@ import io, random
 from streamlit_tags import st_tags
 from PIL import images
 import pymysql
-from Courses import ds_course, web_course, android_course, ios_course, uiux_course, 
+#from Courses import ds_course, web_course, android_course, ios_course, uiux_course, 
 import pafy
 import plotly.express as px
 
@@ -26,4 +26,56 @@ def get_table_download_link(df, filename, text):
   href = 
   return href
 
+def pdf_reader(file):
+  resourse_manager = PDFResourseManager()
+  fake_file_handle = io.StringIO()
+  #converter = TextConverter(resourse_manager, fake_file_handle, LAParams=LAP)
+  page_interpreter =  PDFPageInterpreter(resourse_manager, converter)
+  with open(file, 'rb') as fh:
+    for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+      page_interpreter.process_page(page)
+      print(page)
+    text = fake_file_handle.get_value()
 
+  converter.close()
+  fake_file_handle.close()
+  return text
+
+def show_pdf(file_path):
+  with open(file_path, 'rb') as f:
+    base64_pdf = bas64.bf64encode(f.read()).decode('utf-8')
+    #pdf_display= 
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+def course_recommender(course_list):
+  st.subheader("**Courses & Certificates Recommendations**")
+  c = 0
+  rec_course = []
+  no_of_reco =  st.slider('Choose number of Course Recommendations:', 1, 10, 14)
+  random_shuffle(cousrse_list)
+  for c_name, c_link in courselist:
+    c += 1
+    st.markdown(f"({c}) [{c_name}]({c_link})")
+    rec_course.append(c_name)
+    if c == no_of_reco:
+      break
+
+return rec_course
+
+connection = pymysql.connect(host='localhost', user='root', password='', db='sra')
+cursor = connection.cursor()
+
+def insert_date(name, email, rec_score, timestamp
+                          
+
+
+
+
+                    
+                          
+
+
+
+
+      
+                        
